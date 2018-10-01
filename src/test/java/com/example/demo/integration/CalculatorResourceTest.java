b1122f13fbe7d45ca32689fc91e0c0f452919699
Murali <murali@Muralis-MacBook-Air.local>
2018-10-01 12:17:34 +0530

    commiting some change

--- src/test/java/com/example/demo/integration/CalculatorResourceTest.java
+++ src/test/java/com/example/demo/integration/CalculatorResourceTest.java
@@ -15,6 +15,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+// added some content
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
