19b06b59360ab679d310dc1143531e3b7648cb11
Murali <murali@Muralis-MacBook-Air.local>
2018-10-02 21:06:38 +0530

    sonarqube added

--- src/test/java/com/example/demo/integration/CalculatorResourceTestIT.java
+++ src/test/java/com/example/demo/integration/CalculatorResourceTestIT.java
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class CalculatorResourceTest {
+public class CalculatorResourceTestIT {
 
 	@Autowired
 	private MockMvc mockMvc;
